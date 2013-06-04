d3b2feb19633d7589ada8b034d4e0f07e6b0ef45
Robin Verweij <robinverweij@hotmail.com>
2013-06-03 17:09:09 -0700

    updated boat stuffs

--- WindowsGame1/WindowsGame1/Boat.cs
+++ WindowsGame1/WindowsGame1/Boat.cs
@@ -18,14 +18,18 @@ namespace WindowsGame1
       public Vector2 m_position;
       public Vector2 m_destination;
       public Texture2D m_texture;
-      private int direction; // 0 north, 1 northeast, 2 east ...
-      private int health;
-      private int dmg;
-      private int range;
+      public int direction; // 0 north, 1 northeast, 2 east ...
+      public int health;
+      public int armor;
+      public int dmg1;
+      public int dmg2;
+      public int dmg3;
+      public int range;
       public int owner;
-      private int cooldown;
-      private int attackTime;
+      public int cooldown;
+      public int attackTime;
 
+      // type: 0 = battleship, 1 = destroyer, 2 = submarine
       public Boat(Vector2 position, Texture2D texture, int type, int owner)
       {
          m_position = position;
@@ -36,19 +40,28 @@ namespace WindowsGame1
          texture.GetData<Color>(colors);
          this.owner = owner;
          cooldown = 0;
+         health = 30;
+         attackTime = 50;
+         range = 20;
          switch (type)
          {
             case 0:
-               health = 20;
-               dmg = 5;
-               attackTime = 50;
-               range = 20;
+               armor = 3;
+               dmg1 = 6;
+               dmg2 = 15;
+               dmg3 = 10;      
                break;
             case 1:
-               health = 10;
-               dmg = 10;
-               attackTime = 50;
-               range = 20;
+               armor = 2;
+               dmg1 = 15;
+               dmg2 = 10;
+               dmg3 = 6;
+               break;
+            case 2:
+               armor = 1;
+               dmg1 = 6;
+               dmg2 = 10;
+               dmg3 = 15;
                break;
          }
       }
@@ -102,19 +115,44 @@ namespace WindowsGame1
          if (cooldown != 0)
             cooldown--;
 
+         Boat closestAttackableBoat = null;
+         float closestBoatDistance = 0.0;
          foreach(Boat b in Boats)
          {
             if (b != this)
             {
                if (b.owner != this.owner && this.cooldown == 0)
                {
-                  if (Vector2.Distance(b.m_position, this.m_position) <= this.range)
+                  float tempDistance = Vector2.Distance(b.m_position, this.m_position);
+                  if (tempDistance <= this.range)
                   {
-                     b.health -= this.dmg;
-                     this.cooldown = this.attackTime;
+                     if (closestAttackableBoat == null) {
+                        closestAttackableBoat = b;
+                        closestBoatDistance = tempDistance;
+                     }
+                     else if (tempDistance < closestBoatDistance) {
+                        closestAttackableBoat = b;
+                        closestBoatDistance = tempDistance;
+                     }
                   }
                }
             }
+         }
+         
+         if (closestAttackableBoat != null) {
+            switch (closestAttackableBoat.armor)
+            {
+               case 1:
+                  closestAttackableBoat.health -= this.dmg1;                        
+                  break;
+               case 2:
+                  closestAttackableBoat.health -= this.dmg2;
+                  break;
+               case 3:
+                  closestAttackableBoat.health -= this.dmg3;
+                  break;
+            }
+            this.cooldown = this.attackTime;
          }
       }
 
